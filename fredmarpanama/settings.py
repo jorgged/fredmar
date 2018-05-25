d725cf4e7966acf53f68cde26493f4474421058d
Jorge Garcia <jdgarcia893@gmail.com>
2018-05-25 16:36:02 -0400

    # static root

--- fredmarpanama/settings.py
+++ fredmarpanama/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # MEDIA_ROOT = os.path.join(ENV_PATH, 'media/inventory/')
 
 
-# STATIC_ROOT =os.path.join(BASE_DIR,'static')
+STATIC_ROOT =os.path.join(BASE_DIR,'static')
 STATIC_URL =os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
